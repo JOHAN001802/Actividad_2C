4fcf67a7a49a0a32adbbaa72d6eeea2a54f06a78
Johan Steven Restrepo Marin <johan.restrepo50953@ucaldas.edu.co>
2023-11-10 23:56:08 -0500

    Ejercicios2C

---
+++ Estructuradecontrol.java
@@ -0,0 +1,15 @@
+public class Estructuradecontrol extends Base{
+
+public static void main(String[] args) {
+    int k=1;
+    int n=0;
+
+    
+ while (k<=3) {
+    screen.println(k);
+    k+=1;
+    screen.println("fin");
+ }
+} 
+    
+}
